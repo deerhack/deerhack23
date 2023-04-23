684920ca3fd93da8b8ebaa39c46719b4d332c29c
Rojen Maharjan <44132059+rojen11@users.noreply.github.com>
2023-04-23 12:09:43 +0545

    fix: responsiveness of caraousel

--- apps/web/src/components/Carousel/Carousel.tsx
+++ apps/web/src/components/Carousel/Carousel.tsx
@@ -17,18 +17,9 @@ const Carousel = ({ carouselCard }: Props) => {
     slidesToScroll: 3,
     initialSlide: 0,
     responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true,
-        },
-      },
 
       {
-        breakpoint: 540,
+        breakpoint: 1200,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
